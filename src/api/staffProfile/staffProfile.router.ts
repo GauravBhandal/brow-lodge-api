ef2b8570188ef0da368211e92b56658b66b1c7a7
carediary <carediary.app@gmail.com>
2022-01-17 01:15:24 +1030

    Removing auth middleware from individual routes

--- src/api/staffProfile/staffProfile.router.ts
+++ src/api/staffProfile/staffProfile.router.ts
@@ -3,11 +3,8 @@ import express from "express";
 import controller from "./staffProfile.controller";
 import staffProfileSchems from "./staffProfile.schema";
 import { catchWrap } from "../../components/errors";
-import authMiddleware from "../../components/auth";
 
 const router = express.Router();
-
-router.use(authMiddleware);
 
 router.post(
   "/",
