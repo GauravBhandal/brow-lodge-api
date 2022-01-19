b2d000bda3760e33780830a69fbc0c8effb88c35
carediary <carediary.app@gmail.com>
2022-01-19 21:00:23 +1030

    Fixing the default page number

--- src/components/paging/index.ts
+++ src/components/paging/index.ts
@@ -1,7 +1,10 @@
 export const DEFAULT_PAGE_SIZE = 500; // TODO: Sending 500 records by default may cause issues
-export const DEFAULT_PAGE_NUMBER = 0;
+export const DEFAULT_PAGE_NUMBER = 1;
 
-export const getPagingParams = (page: number, pageSize: number) => {
+export const getPagingParams = (
+  page: number = DEFAULT_PAGE_NUMBER,
+  pageSize: number
+) => {
   const limit = pageSize || DEFAULT_PAGE_SIZE;
   const offset = page ? (page - 1) * limit : DEFAULT_PAGE_NUMBER;
 
