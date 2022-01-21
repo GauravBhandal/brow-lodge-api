8fef10e4368f98809a39948feae9a5d3241f5633
LAPTOP-OEHG4BTR\Hp <bhandalgaurav1999@gmail.com>
2022-01-21 10:25:04 +0530

    CD-214 filter improvements

--- src/components/filters/index.ts
+++ src/components/filters/index.ts
@@ -30,25 +30,32 @@ export const getFilters = (whereProps: any = {}) => {
   Object.keys(whereProps).forEach((prop) => {
     //Loop through every where prop
     const subStrings = prop.split("_"); //split the where prop in substring by key and operation
+    const nestedModal = subStrings[0] && subStrings[0].split(".");
+    let outerFilterKey = "primaryFilters"; //For nested filter give normal filter in data key and other in that specific key
+    let innerFilterKey = subStrings[0];
 
     const applyOr = Array.isArray(whereProps[prop]); //Check if same key is used for multiple where props
     let filterOperations;
+    if (nestedModal.length && nestedModal.length > 1) {
+      outerFilterKey = nestedModal[0];
+      innerFilterKey = nestedModal[1];
+    }
+    filters[outerFilterKey] = { ...filters[outerFilterKey] };
     if (applyOr) {
       //Apply or operation if same key is used for multiple where props
       whereProps[prop].forEach((item: any) => {
         filterOperations = getFilterOperations(subStrings[1], item);
         orFilterForSameKey = [...orFilterForSameKey, filterOperations];
       });
-      filters[subStrings[0]] = { [Op.or]: orFilterForSameKey };
+      filters[outerFilterKey][innerFilterKey] = { [Op.or]: orFilterForSameKey };
     } else {
       //Other wise just apply the operations applied to where prop
       filterOperations = getFilterOperations(subStrings[1], whereProps[prop]);
-      filters[subStrings[0]] = {
-        ...filters[subStrings[0]],
+      filters[outerFilterKey][innerFilterKey] = {
+        ...filters[outerFilterKey][innerFilterKey],
         ...filterOperations,
       };
     }
   });
-
   return filters;
 };
