3a06bd8f7e2defab630ce9541ccbab970408b75d
carediary <yasmine.kaur@resolution.systems>
2022-01-27 01:22:21 +1030

    bug fixing in attachment

--- src/api/attachment/attachment.router.ts
+++ src/api/attachment/attachment.router.ts
@@ -4,27 +4,19 @@ import controller from "./attachment.controller";
 import attachmentSchems from "./attachment.schema";
 import { catchWrap } from "../../components/errors";
 import uploadMiddleware from "../../components/upload";
-import { canDo } from "../../components/ability";
 
 const router = express.Router();
 
-router.post(
-  "/",
-  canDo("create", "attachment"),
-  uploadMiddleware,
-  catchWrap(controller.createAttachment)
-);
+router.post("/", uploadMiddleware, catchWrap(controller.createAttachment));
 
 router.delete(
   "/:attachmentId",
-  canDo("delete", "attachment"),
   attachmentSchems.deleteAttachment,
   catchWrap(controller.deleteAttachment)
 );
 
 router.get(
   "/:attachmentId",
-  canDo("read", "attachment"),
   attachmentSchems.getAttachmentById,
   catchWrap(controller.getattachmentById)
 );
