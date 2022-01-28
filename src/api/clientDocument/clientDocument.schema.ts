107578ff39dff195e45eb1391b4250659fa10f0c
carediary <carediary.app@gmail.com>
2022-01-28 15:59:04 +1030

    Expiry is optional

--- src/api/clientDocument/clientDocument.schema.ts
+++ src/api/clientDocument/clientDocument.schema.ts
@@ -7,7 +7,7 @@ const createClientDocumentSchema = wrapSchema({
   body: Joi.object().keys({
     comments: Joi.string().allow("", null),
     hasExpiry: Joi.boolean().required(),
-    expiryDate: Joi.date().required(),
+    expiryDate: Joi.date(),
     client: requiredUUIDSchema(),
     category: requiredUUIDSchema(),
     type: requiredUUIDSchema(),
@@ -22,7 +22,7 @@ const editClientDocumentSchema = wrapSchema({
   body: Joi.object().keys({
     comments: Joi.string().allow("", null),
     hasExpiry: Joi.boolean().required(),
-    expiryDate: Joi.date().required(),
+    expiryDate: Joi.date(),
     client: requiredUUIDSchema(),
     category: requiredUUIDSchema(),
     type: requiredUUIDSchema(),
