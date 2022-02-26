e0fb4238eabbe0a334e329a1744346d9ef5bf9ac
carediary <carediary.app@gmail.com>
2022-02-26 13:55:51 +1030

    Migration file cleanup

--- migrations/20220213125116-roster.js
+++ migrations/20220213125116-roster.js
@@ -33,9 +33,55 @@ CREATE TABLE IF NOT EXISTS "teams_client_profiles" (
   PRIMARY KEY ("id")
 );
 ALTER TABLE "teams_client_profiles" ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS "shift_types" (
+  "id" UUID NOT NULL,
+  "name" VARCHAR NOT NULL,
+  "company" UUID NOT NULL REFERENCES "companies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "shift_types" ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS "shift_records" (
+  "id" UUID NOT NULL,
+  "start_date_time" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "end_date_time" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "staff" UUID REFERENCES "staff_profiles" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "client" UUID REFERENCES "client_profiles" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "company" UUID NOT NULL REFERENCES "companies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "shift_records" ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS "shift_records_shift_types" (
+  "id" UUID NOT NULL,
+  "start_time" TIME WITHOUT TIME ZONE NOT NULL,
+  "shift" UUID REFERENCES "shift_records" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "type" UUID REFERENCES "shift_types" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "shift_records_shift_types" ENABLE ROW LEVEL SECURITY;
 `;
 
 const queryDown = `
+ALTER TABLE "shift_records_shift_types" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "shift_records_shift_types";
+
+ALTER TABLE "shift_records" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "shift_records";
+
+ALTER TABLE "shift_types" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "shift_types";
+
 ALTER TABLE "teams_client_profiles" DISABLE ROW LEVEL SECURITY;
 DROP TABLE IF EXISTS "teams_client_profiles";
 
