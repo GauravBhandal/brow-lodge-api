3dfbbb084864f1c2fa7a61f87e5d6cac0b82f1c8
LAPTOP-OEHG4BTR\Hp <bhandalgaurav1999@gmail.com>
2022-02-27 10:55:29 +0530

    CD-279 Changed the error messages in document category and types

--- src/api/clientDocumentCategory/clientDocumentCategory.error.ts
+++ src/api/clientDocumentCategory/clientDocumentCategory.error.ts
@@ -1,7 +1,7 @@
 enum ClientDocumentCategoryErrorCode {
   CATEGORY_NOT_FOUND = "CATEGORY_NOT_FOUND",
   CATEGORY_ALREADY_EXISTS = "CATEGORY_ALREADY_EXISTS",
-  CATEGORY_CONTAINS_DUPLICATE_TYPE = "CATEGORY_CONTAINS_DUPLICATE_TYPE",
+  CATEGORY_CONTAINS_DUPLICATE_TYPE = "Category contains duplicate documents",
 }
 
 export default ClientDocumentCategoryErrorCode;
