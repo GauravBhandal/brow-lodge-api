88fbf304a265663ef746f7e2360036fca45d323e
LAPTOP-OEHG4BTR\Hp <bhandalgaurav1999@gmail.com>
2022-04-08 08:24:15 +0530

    CD-371 changed the file name

--- migrations/20220408011411-company-improvements.js
+++ migrations/20220408011411-company-improvements.js
@@ -1,16 +1,19 @@
 "use strict";
 
 const queryUp = `
-ALTER TABLE "companies" ADD COLUMN 
-  "website" VARCHAR, ADD COLUMN 
-"email" VARCHAR, ADD COLUMN 
-"ndis_registration_number" VARCHAR, ADD COLUMN 
-"timezone" VARCHAR;
+ALTER TABLE "companies" 
+ADD COLUMN "website" VARCHAR,
+ADD COLUMN "email" VARCHAR,
+ADD COLUMN "ndis_registration_number" VARCHAR,
+ADD COLUMN "timezone" VARCHAR;
 `;
 
 const queryDown = `
 ALTER TABLE "companies"
-  DROP COLUMN "website",DROP COLUMN "email",DROP COLUMN "ndis_registration_number",DROP COLUMN "timezone";
+DROP COLUMN "website",
+DROP COLUMN "email",
+DROP COLUMN "ndis_registration_number",
+DROP COLUMN "timezone";
 `;
 
 module.exports = {
