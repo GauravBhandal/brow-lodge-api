9ac6ba6020e2a7e45415349d17cdde21fccf4d62
carediary <carediary.app@gmail.com>
2022-05-05 21:21:07 +0930

    Code review

---
+++ src/api/integration/integrationExternalData/index.ts
@@ -0,0 +1,11 @@
+import IntegrationExternalDataErrorCode from "./integrationExternalData.error";
+import IntegrationExternalDataModel from "./integrationExternalData.model";
+import { IntegrationExternalData } from "./integrationExternalData.types";
+import integrationExternalDataService from "./integrationExternalData.service";
+
+export {
+  IntegrationExternalDataErrorCode,
+  IntegrationExternalDataModel,
+  IntegrationExternalData,
+  integrationExternalDataService,
+};
