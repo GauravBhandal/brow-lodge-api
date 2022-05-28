3108399cb93f3c2af8e9591dbcd374589a89d5ac
carediary <carediary.app@gmail.com>
2022-05-28 15:27:14 +0930

    Code Review

--- src/api/clientProfile/clientContact/index.ts
+++ src/api/clientProfile/clientContact/index.ts
@@ -1,11 +1,5 @@
-import ClientContactErrorCode from "./clientContact.error";
 import ClientContactModel from "./clientContact.model";
 import { ClientContact } from "./clientContact.types";
 import clientContactService from "./clientContact.service";
 
-export {
-  ClientContactErrorCode,
-  ClientContactModel,
-  ClientContact,
-  clientContactService,
-};
+export { ClientContactModel, ClientContact, clientContactService };
