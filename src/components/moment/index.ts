347478ab94e2764ee7ab105d1565b820f351904f
carediary <carediary.app@gmail.com>
2022-07-14 00:28:13 +0930

    CD-483-remove-hardcoded-timezone code review

--- src/components/moment/index.ts
+++ src/components/moment/index.ts
@@ -2,7 +2,7 @@ import moment from "moment-timezone";
 
 const makeMoment = (date?: any, timezone?: string) =>
   date
-    ? moment.tz(date, timezone || "Australia/Adealide")
-    : moment.tz(timezone || "Australia/Adealide");
+    ? moment.tz(date, timezone || "Australia/Adelaide")
+    : moment.tz(timezone || "Australia/Adelaide");
 
 export default makeMoment;
