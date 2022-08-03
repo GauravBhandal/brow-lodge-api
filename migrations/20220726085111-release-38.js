3521a46b1c3c2204fb5396535e9fb31c16e26475
carediary <carediary.app@gmail.com>
2022-08-04 00:01:46 +0930

    CD-522-expense-form-clean-up

--- migrations/20220726085111-release-38.js
+++ migrations/20220726085111-release-38.js
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS "expenses" (
   "status" VARCHAR NOT NULL,
   "payment_reimbursed" VARCHAR NOT NULL,
   "company" UUID NOT NULL REFERENCES "companies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "temp_column" UUID,
   "created" TIMESTAMP WITH TIME ZONE NOT NULL,
   "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
   "deleted" TIMESTAMP WITH TIME ZONE,
@@ -29,6 +30,32 @@ CREATE TABLE IF NOT EXISTS "expenses_attachments" (
   PRIMARY KEY ("id")
 );
 ALTER TABLE "expenses_attachments" ENABLE ROW LEVEL SECURITY;
+
+
+INSERT INTO expenses(id, date, staff, total_expense, description, paid_by, status, payment_reimbursed, company, temp_column, created, updated)
+SELECT uuid_generate_v4(), date, staff, total_cost, description, 'Company', 'Pending', 'Not Applicable', company, id, created, updated FROM company_expenses;
+
+INSERT INTO expenses_attachments(id, relation, attachment, created, updated)
+SELECT uuid_generate_v4(), e.id, cea.attachment, cea.created, cea.updated FROM expenses as e 
+INNER JOIN company_expenses_attachments as cea ON e.temp_column = cea.relation;
+
+
+INSERT INTO expenses(id, date, staff, total_expense, description, paid_by, status, payment_reimbursed, company, temp_column, created, updated)
+SELECT uuid_generate_v4(), date, staff, total_cost, description, 'Staff', 'Pending', 'No', company, id, created, updated FROM expense_reimbursements;
+
+INSERT INTO expenses_attachments(id, relation, attachment, created, updated)
+SELECT uuid_generate_v4(), e.id, era.attachment, era.created, era.updated FROM expenses as e 
+INNER JOIN expense_reimbursements_attachments as era ON e.temp_column = era.relation;
+
+
+INSERT INTO expenses(id, date, staff, client, total_expense, description, paid_by, status, payment_reimbursed, company, temp_column, created, updated)
+SELECT uuid_generate_v4(), date, staff, client, total_expense, '', 'Participant', 'Pending', 'Not Applicable', company, id, created, updated FROM participant_expenses;
+
+INSERT INTO expenses_attachments(id, relation, attachment, created, updated)
+SELECT uuid_generate_v4(), e.id, pea.attachment, pea.created, pea.updated FROM expenses as e 
+INNER JOIN participant_expenses_attachments as pea ON e.temp_column = pea.relation;
+
+ALTER TABLE "expenses" DROP COLUMN "temp_column";
 `;
 
 const queryDown = `
