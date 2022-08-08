a293b7d31b9724f64d8a4bddce38ad4dfb6ae5e9
pavi <pavi@carediary.com.au>
2022-08-08 10:18:04 +0930

    CD-575 Code review

--- migrations/20220806114556-release-39.js
+++ migrations/20220806114556-release-39.js
@@ -3,29 +3,23 @@
 const queryUp = `
 
 DROP TABLE IF EXISTS "company_expenses_attachments";
-
 DROP TABLE IF EXISTS "company_expenses";
 
 DROP TABLE IF EXISTS "participant_expenses_attachments";
-
 DROP TABLE IF EXISTS "participant_expenses";
 
 DROP TABLE IF EXISTS "expense_reimbursements_attachments";
-
 DROP TABLE IF EXISTS "expense_reimbursements";
 `;
 
-const queryDown = `
-
-
-`;
+const queryDown = ``;
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface) => {
     await queryInterface.sequelize.query(queryUp);
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.sequelize.query(queryDown);
   },
 };
