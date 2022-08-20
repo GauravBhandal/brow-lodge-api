ab2b71f800707b12bcb81e4bf236bfc97a209ca8
pavi <pavi@carediary.com.au>
2022-08-20 11:00:23 +0930

    CD-600 Add company name and user email in API logs

--- src/config/express.ts
+++ src/config/express.ts
@@ -13,6 +13,7 @@ import { handleErrorMiddleware, shouldReportError } from "../components/errors";
 import modelsAssociations from "../components/sequelize/model.associations";
 import joiErrorMiddleware from "../components/joi/middleware";
 import rateLimitMiddleware from "../components/rateLimiter";
+import authMiddleware from "../components/auth";
 
 export default function (app: Express) {
   // TODO: Currently, allowing all CROS requests
@@ -20,7 +21,13 @@ export default function (app: Express) {
   // const corsOptions = {
   //   allowedHeaders,
   // };
-  const morganConfig = `:method :url (:response-time ms) :status`;
+  morgan.token("user-email", function (req: any, res) {
+    return req?.auth?.userEmail || "";
+  });
+  morgan.token("company-name", function (req: any, res) {
+    return req?.auth?.companyName || "";
+  });
+  const morganConfig = `:method :url (:response-time ms) :status :company-name :user-email`;
 
   // Test DB
   sequelize
@@ -37,7 +44,6 @@ export default function (app: Express) {
 
   app.use(rateLimitMiddleware);
   app.use(helmet());
-  app.use(morgan(morganConfig));
   app.use(compressionMiddleware());
   app.use(cors()); //TODO: Currently, allowing all CROS requests
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,6 +56,8 @@ export default function (app: Express) {
 
   modelsAssociations.initialize();
 
+  app.use(authMiddleware);
+  app.use(morgan(morganConfig));
   app.use((config.URL_PREFIX || "") + "/", router);
 
   // The error handler must be before any other error middleware and after all controllers
