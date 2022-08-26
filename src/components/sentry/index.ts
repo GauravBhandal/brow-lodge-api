3b66256210ebbb9ff22da635f523d58fb8994748
pavi <pavi@carediary.com.au>
2022-08-26 10:27:57 +0930

    CD-587 Don't send sentry in localhost

--- src/components/sentry/index.ts
+++ src/components/sentry/index.ts
@@ -15,6 +15,7 @@ export function initialiseSentry(app: Express) {
       new Tracing.Integrations.Express({ app }),
     ],
 
+    enabled: config.APP_ENV !== "localhost",
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
