7b5be46ebcf68e0506565455dbb1fbdd6fa2d1cb
pavi <pavi@carediary.com.au>
2022-10-01 20:45:21 +0930

    Migration file cleanup

--- migrations/20220806114556-release-39.js
+++ migrations/20220806114556-release-39.js
@@ -32,41 +32,9 @@ CREATE TABLE IF NOT EXISTS "participant_goals" (
   PRIMARY KEY ("id")
 );
 ALTER TABLE "participant_goals" ENABLE ROW LEVEL SECURITY;
-
-CREATE TABLE IF NOT EXISTS "external_contracts" (
-  "id" UUID NOT NULL,
-  "date" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "next_review_date" TIMESTAMP WITH TIME ZONE,
-  "name" VARCHAR (255) NOT NULL,
-  "notes" VARCHAR,
-  "staff" UUID NOT NULL REFERENCES "staff_profiles" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-  "company" UUID NOT NULL REFERENCES "companies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "deleted" TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY ("id")
-);
-ALTER TABLE "external_contracts" ENABLE ROW LEVEL SECURITY;
-
-CREATE TABLE IF NOT EXISTS "external_contracts_attachments" (
-  "id" UUID NOT NULL,
-  "relation" UUID NOT NULL REFERENCES "external_contracts" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-  "attachment" UUID NOT NULL REFERENCES "attachments" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "deleted" TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY ("id")
-);
-ALTER TABLE "external_contracts_attachments" ENABLE ROW LEVEL SECURITY;
 `;
 
 const queryDown = `
-ALTER TABLE "external_contracts_attachments" DISABLE ROW LEVEL SECURITY;
-DROP TABLE IF EXISTS "external_contracts_attachments";
-
-ALTER TABLE "external_contracts" DISABLE ROW LEVEL SECURITY;
-DROP TABLE IF EXISTS "external_contracts";
-
 ALTER TABLE "participant_goals" DISABLE ROW LEVEL SECURITY;
 DROP TABLE IF EXISTS "participant_goals";
 `;
--- migrations/20221001110744-release-47.js
+++ migrations/20221001110744-release-47.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const queryUp = `
-
 CREATE TABLE IF NOT EXISTS "progress_reports_attachments" (
   "id" UUID NOT NULL,
   "relation" UUID NOT NULL REFERENCES "progress_reports" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
@@ -12,9 +11,41 @@ CREATE TABLE IF NOT EXISTS "progress_reports_attachments" (
   PRIMARY KEY ("id")
 );
 ALTER TABLE "progress_reports_attachments" ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS "external_contracts" (
+  "id" UUID NOT NULL,
+  "date" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "next_review_date" TIMESTAMP WITH TIME ZONE,
+  "name" VARCHAR (255) NOT NULL,
+  "notes" VARCHAR,
+  "staff" UUID NOT NULL REFERENCES "staff_profiles" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "company" UUID NOT NULL REFERENCES "companies" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "external_contracts" ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE IF NOT EXISTS "external_contracts_attachments" (
+  "id" UUID NOT NULL,
+  "relation" UUID NOT NULL REFERENCES "external_contracts" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "attachment" UUID NOT NULL REFERENCES "attachments" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "external_contracts_attachments" ENABLE ROW LEVEL SECURITY;
 `;
 
 const queryDown = `
+ALTER TABLE "external_contracts_attachments" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "external_contracts_attachments";
+
+ALTER TABLE "external_contracts" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "external_contracts";
+
 ALTER TABLE "progress_reports_attachments" DISABLE ROW LEVEL SECURITY;
 DROP TABLE IF EXISTS "progress_reports_attachments";
 `;
