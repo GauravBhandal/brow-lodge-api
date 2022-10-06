b0f2b153e8246ec9fd8badfdf7b75597d6a63a18
Neha <nehamittal4893@gmail.com>
2022-10-06 06:52:14 +0530

    CD-654-crud-medication-register

---
+++ src/api/medicationRegister/medicationRegister.error.ts
@@ -0,0 +1,5 @@
+enum MedicationRegisterErrorCode {
+  MEDICATION_REGISTER_NOT_FOUND = "Medication register not found!",
+}
+
+export default MedicationRegisterErrorCode;
---
+++ src/api/medicationRegister/medicationRegister.model.ts
@@ -0,0 +1,62 @@
+import Sequelize from "sequelize";
+import { pick as _pick } from "lodash";
+
+import modelManager, {
+  CommonSequelizeModel,
+} from "../../components/sequelize/manager";
+import {
+  MedicationRegister,
+  CreateMedicationRegisterProps,
+} from "./medicationRegister.types";
+
+class MedicationRegisterModel<
+    ModelAttributes = MedicationRegister,
+    ModelCreationAttributes = CreateMedicationRegisterProps
+  >
+  extends CommonSequelizeModel<ModelAttributes, ModelCreationAttributes>
+  implements MedicationRegister
+{
+  startDate!: MedicationRegister["startDate"];
+  endDate: MedicationRegister["endDate"];
+  staff!: MedicationRegister["staff"];
+  Staff: MedicationRegister["Staff"];
+  client!: MedicationRegister["client"];
+  Client: MedicationRegister["Client"];
+  medicationName!: MedicationRegister["medicationName"];
+  administrationType!: MedicationRegister["administrationType"];
+  dosage!: MedicationRegister["dosage"];
+  frequency!: MedicationRegister["frequency"];
+  isPrescribed: MedicationRegister["isPrescribed"];
+  notes: MedicationRegister["notes"];
+  nextReviewDate: MedicationRegister["nextReviewDate"];
+  company!: MedicationRegister["company"];
+  Company: MedicationRegister["Company"];
+}
+
+modelManager.init(
+  "MedicationRegister",
+  MedicationRegisterModel,
+  {
+    startDate: { type: Sequelize.DATE, allowNull: false },
+    endDate: { type: Sequelize.DATE },
+    medicationName: { type: Sequelize.STRING, allowNull: false },
+    administrationType: { type: Sequelize.STRING, allowNull: false },
+    dosage: { type: Sequelize.STRING, allowNull: false },
+    frequency: { type: Sequelize.STRING, allowNull: false },
+    isPrescribed: { type: Sequelize.STRING },
+    notes: { type: Sequelize.STRING },
+    nextReviewDate: { type: Sequelize.DATE },
+  },
+  {
+    defaultScope: {
+      attributes: {
+        exclude: ["deleted"],
+      },
+    },
+    underscored: true,
+    paranoid: true,
+    tableName: "medication_registers",
+  }
+);
+
+export default MedicationRegisterModel;
