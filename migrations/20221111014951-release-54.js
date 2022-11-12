c8dfb0e8e116cd274bd869fc9fe68632cd1bc7a7
Gaurav Bhandal <gaurav@carediary.com.au>
2022-11-12 08:48:09 +0530

    CD-678 fixed on release change

--- migrations/20221111014951-release-54.js
+++ migrations/20221111014951-release-54.js
@@ -12,9 +12,23 @@ ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE "templates" 
 ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE TABLE IF NOT EXISTS "service_delivery_attachments" (
+  "id" UUID NOT NULL,
+  "relation" UUID NOT NULL REFERENCES "service_deliveries" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "attachment" UUID NOT NULL REFERENCES "attachments" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  "created" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "updated" TIMESTAMP WITH TIME ZONE NOT NULL,
+  "deleted" TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY ("id")
+);
+ALTER TABLE "service_delivery_attachments" ENABLE ROW LEVEL SECURITY;
 `;
 
 const queryDown = `
+ALTER TABLE "service_delivery_attachments" DISABLE ROW LEVEL SECURITY;
+DROP TABLE IF EXISTS "service_delivery_attachments";
+
 ALTER TABLE "templates"
 DROP COLUMN "archived";
 
