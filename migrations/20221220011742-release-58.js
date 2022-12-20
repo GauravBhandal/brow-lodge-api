1f6ed1f4eecceb3856bb88b2456709cd3391dc6e
Neha <nehamittal4893@gmail.com>
2022-12-20 06:54:34 +0530

    CD-713-add-archive-and-permanent-delete-functionality-in-maintenance-log-form

---
+++ migrations/20221220011742-release-58.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const queryUp = `
+ALTER TABLE "maintenance_logs" 
+ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
+
+`;
+
+const queryDown = `
+
+ALTER TABLE "maintenance_logs"
+DROP COLUMN "archived";
+
+`;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(queryUp);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(queryDown);
+  },
+};
