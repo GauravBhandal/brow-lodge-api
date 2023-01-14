42317782cfad4998711be9577c2be677c9e295e7
Gaurav Bhandal <gaurav@carediary.com.au>
2023-01-14 11:57:21 +0530

    CD-720 fixed migrations

--- migrations/20230114050400-release-62.js
+++ migrations/20230114050400-release-62.js
@@ -7,9 +7,15 @@ ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE "doctor_visits" 
 ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
 
+ALTER TABLE "injury_reports" 
+ADD COLUMN "archived" BOOLEAN NOT NULL DEFAULT FALSE;
+
 `;
 
 const queryDown = `
+ALTER TABLE "injury_reports"
+DROP COLUMN "archived";
+
 ALTER TABLE "doctor_visits"
 DROP COLUMN "archived";
 
