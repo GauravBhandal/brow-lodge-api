90efcb0ad92cd1c2b2ecc169756f765ce9378eff
Gaurav Bhandal <gaurav@carediary.com.au>
2023-02-10 07:46:53 +0530

    CD-766 added down query

--- migrations/20230209181702-release-66.js
+++ migrations/20230209181702-release-66.js
@@ -2,12 +2,15 @@
 
 const queryUp = `
 INSERT INTO incident_types VALUES
-  ('aef069ae-d7c2-45fa-ad72-ff17a3f84511', 'Witnessed fall of the participant', true, now(), now()),
-  ('8af9c40c-8e1a-476d-98c0-d952840d86d5', 'Unwitnessed fall of the participant', true, now(), now());
+  ('aef069ae-d7c2-45fa-ad72-ff17a3f84511', 'Witnessed fall of the participant', TRUE, now(), now()),
+  ('8af9c40c-8e1a-476d-98c0-d952840d86d5', 'Unwitnessed fall of the participant', TRUE, now(), now());
 `;
 
 const queryDown = `
-
+DELETE FROM "incident_types" WHERE id IN(
+  'aef069ae-d7c2-45fa-ad72-ff17a3f84511',
+  '8af9c40c-8e1a-476d-98c0-d952840d86d5'
+  );
 `;
 
 module.exports = {
